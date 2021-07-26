9f07b69904f1bff01a46e48f5a1af4b38ed87d3c
DhrubaDC1 <45314506+DhrubaDC1@users.noreply.github.com>
2021-07-26 21:34:26 +0600

    updated

--- pre-process/preporcessing.py
+++ pre-process/preporcessing.py
@@ -18,11 +18,12 @@ for i in range(len(cat)):
     df_tmp=df[df['timestamp_day']==cat[i]]
     df_group =df_tmp.groupby(['movieId']).count().reset_index().rename(columns={'userId': 'label'}) #get count by time stamp
     df_group['label_nom']=df_group['label']/np.sum(df_group['label'])
-    print(df_group.head(3))
+    #print(df_group.head(3))
     cols = ['movieId']
     ext = ['label','label_nom']
     df_tmp= df_tmp.join(df_group.set_index(cols)[ext], on=cols)
-    print(df_tmp.head(3))
+    print(len(cat)-i)
+    #print(df_tmp.head(3))
 
     ######Save df_tmp #######
     # if file does not exist write header
